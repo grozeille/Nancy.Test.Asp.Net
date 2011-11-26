cbb68c4a9772fa8228908e5dfedb48b98774a9ef
Mathias Kluba <mathias.kluba@gmail.com>
2011-11-26 01:02:21 +0100

    first commit

---
+++ Nancy.Test.Asp.Net/Application/MyApplicationModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject.Modules;
+using Nancy.Test.Asp.Net.Services;
+
+namespace Nancy.Test.Asp.Net.Application
+{
+    public class MyApplicationModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<IService>().To<Service>();
+        }
+    }
+}
---
+++ Nancy.Test.Asp.Net/Services/IService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Test.Asp.Net.Services
+{
+    public interface IService
+    {
+        string SayHello(string name);
+    }
+}
